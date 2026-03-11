3a2ac7b1777ddc161a6ab6b93d2c0fc7fdf2c2c8
Pasidi <167764476+Pasidi-hash@users.noreply.github.com>
2026-03-11 17:39:53 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,10 @@ import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "monitoring.db")
+SCRIPT_PATH = os.path.join(BASE_DIR, "run_tests.py"
+
 @app.get("/")
 def consignes():
      return render_template('consignes.html')
@@ -21,30 +25,26 @@ if __name__ == "__main__":
 def test_dashboard():
     history = []
     history_reversed = []
-    
     try:
         conn = sqlite3.connect(DB_PATH)
         conn.row_factory = sqlite3.Row
-        # Récupère les 30 derniers résultats
+        # On récupère les 30 derniers tests
         history = conn.execute("SELECT * FROM runs ORDER BY timestamp DESC LIMIT 30").fetchall()
         conn.close()
-        
-        # Inversion pour l'ordre chronologique du graphique
         history_reversed = list(reversed(history))
     except Exception as e:
-        print(f"Erreur DB: {e}")
+        print(f"Erreur de lecture DB : {e}")
 
     return render_template("index.html", runs=history, chart_data=history_reversed)
 
 @app.route('/run-manual-test')
 def run_manual_test():
     try:
-        # Exécute le script de test comme si on le lançait en console
+        # On force l'exécution du script python
         subprocess.run(["python3", SCRIPT_PATH], check=True)
     except Exception as e:
-        print(f"Erreur lors du test manuel: {e}")
+        print(f"Erreur lors de l'exécution du test : {e}")
     
-    # Redirige vers le dashboard pour voir le nouveau point
     return redirect(url_for('test_dashboard'))
 
 if __name__ == "__main__":
